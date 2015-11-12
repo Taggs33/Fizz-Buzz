12b604a9b476a588d5e10af18c3fdc84cb6c2831
Taggs33 <taggart3317@gmail.com>
2015-11-12 17:54:47 -0500

    correct js and add css to content

--- js/fizz.js
+++ js/fizz.js
@@ -1,14 +1,21 @@
-$(document).ready(function)() {
+$(document).ready(function() {
 
-var i=1, i <= 100, i++)
-{
-	if (i % 15 == 0)
-		console.log("FizzBuzz");
-	else if (i % 3 == 0)
-		console.log("FIzz");
-	else if (i % 5 == 0)
-		console.log("Buzz")
-	else 
-		console.log(i);
-};
-}
+  for (i = 1; i <= 100; i++) {
+	if (i % 3 == 0 && i % 5 == 0) {
+	  $('.fizzbuzz').append("FizzBuzz<br>");	
+	  console.log("FizzBuzz");
+	}
+	else if (i % 3 == 0) {
+	  $('.fizzbuzz').append("<span class='fizz-red'>Fizz</span><br>");	
+	  console.log("Fizz");
+	}
+	else if (i % 5 == 0) {
+	  $('.fizzbuzz').append("<span class='buzz-blue'>Buzz</span><br>");	
+	  console.log("Buzz");
+	}
+	else {
+	  $('.fizzbuzz').append(i + "<br>");	
+      console.log(i);
+	}
+  }
+});
